1f007202e4404580a37d9499cb66c0ab682c245e
Bùi Chính Hiệp <chinhhiep996@gmail.com>
2019-12-21 17:20:38 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO; // types for managing the filesystem
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+namespace DotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputFileSystemInfo();
+        }
+
+        static void OutputFileSystemInfo()
+        {
+            WriteLine("{0,-33} {1}", "Path.PathSeparator", PathSeparator);
+            WriteLine("{0,-33} {1}", "Path.DirectorySeparatorChar", DirectorySeparatorChar);
+            WriteLine("{0,-33} {1}", "Directory.GetCurrentDirectory()", GetCurrentDirectory());
+            WriteLine("{0,-33} {1}", "Environment.CurrentDirectory", CurrentDirectory);
+            WriteLine("{0,-33} {1}", "Environment.SystemDirectory", SystemDirectory);
+            WriteLine("{0,-33} {1}", "Path.GetTempPath()", GetTempPath());
+            WriteLine("GetFolderPath(SpecialFolder");
+            WriteLine("{0,-33} {1}", "  .System)", GetFolderPath(SpecialFolder.System));
+            WriteLine("{0,-33} {1}", "  .ApplicationData)", GetFolderPath(SpecialFolder.ApplicationData));
+            WriteLine("{0,-33} {1}", "  .MyDocuments)", GetFolderPath(SpecialFolder.MyDocuments));
+            WriteLine("{0,-33} {1}", "  .Personal)", GetFolderPath(SpecialFolder.Personal));
+        }
+    }
+}
